d09c72c4687b8dd0163e8ee8a1036a2eea7eeb59
Amit Panda <30612251+amitpanda365@users.noreply.github.com>
2024-07-19 22:53:48 +0530

    Create Jumper.java

---
+++ SnakeAndLadder/Jumper.java
@@ -0,0 +1,20 @@
+package SnakeLadder;
+
+public class Jumper {
+
+    private int start;
+    private int end;
+
+    public Jumper(int start, int end) {
+        this.start = start;
+        this.end = end;
+    }
+
+    public int getStart() {
+        return start;
+    }
+
+    public int getEnd() {
+        return end;
+    }
+}
